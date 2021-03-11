509bfb84a9ef1feb65c2a24cb9429b7fae85dcc1
Anita Mathews <anita.rachel.mathews@gmail.com>
2021-03-11 15:48:29 -0500

    added rgeos

--- init.R
+++ init.R
@@ -1,4 +1,4 @@
-my_packages = c("shinydashboard", "leaflet")
+my_packages = c("shinydashboard", "leaflet", "rgeos", "dplyr", "geojson", "geojsonio")
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
